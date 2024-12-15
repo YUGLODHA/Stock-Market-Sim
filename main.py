0f28da978180678ff3ede37cecb32e808536010e
Yug Lodha <yugshitalkumarlodha@gmail.com>
2024-12-15 15:12:40 +0530

    15-12-24-15-12

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import os
 import json
+import time
 
 SAVESFOLDER = "saves/"
 
@@ -19,7 +20,7 @@ def save(savename, name, balance, stocks, dob, email):
 def load(path):
 	with open(path, 'r') as file:
 		data = json.load(file)
-	return data["name"], data["balance"], data["stocks"], data["dob"], data["email"], data["phoneno"]
+	return data["savename"], data["name"], data["balance"], data["stocks"], data["dob"], data["email"], data["phoneno"]
 
 if __name__ == '__main__':
 	# Load File and Save File
@@ -29,7 +30,7 @@ if __name__ == '__main__':
 		print("This is a text-based simulation of a simple stock market. You can buy, sell, and manage your portfolio.")
 		print("Please fill in the following details(they are not nessary and can be random):")
 		name=input("Name Of Your Portfolio: ")
-		balance=10000.00
+		balance=1000.00
 		stocks=[]
 		dob=input("Date Of Birth(dd/mm/yyyy): ")
 		email=input("Email Address: ")
@@ -37,25 +38,58 @@ if __name__ == '__main__':
 		savename=input("Name For Your Save File: ")
 		save(savename, name, balance, stocks, dob, email)
 	elif len(os.listdir(SAVESFOLDER))==1:
-		name, balance, stocks, dob, email, phoneno = load(SAVESFOLDER+os.listdir(SAVESFOLDER)[0])
+		savename, name, balance, stocks, dob, email, phoneno = load(SAVESFOLDER+os.listdir(SAVESFOLDER)[0])
 	else:
 		file = input("Enter The Name of The Save You Want To Load: ")
 		fileLoc = SAVESFOLDER+file+".json"
-		name, balance, stocks, dob, email, phoneno = load(fileLoc)
+		savename, name, balance, stocks, dob, email, phoneno = load(fileLoc)
 
 	# Main Loop
 	while True:
-			os.system("cls")
-			print("=================================Welcome to the Stock Market Simulator=================================")
-			print(f"Portfolio: {name}")
-			print(f"Current Balance: ${balance:.2f}")
-			print("Current Stocks:", end="")
-			for stock in stocks:
-				print("Stock: {stock[0]}:{stock[1]}", end="")
+			os.system('cls')
+			print("=================================The Stock Market Simulator=================================")
 			print("\nOptions:")
 			print("1. Buy Stock")
 			print("2. Sell Stock")
 			print("3. View Portfolio")
 			print("4. Update Personal Information")
-			print("5. Exit")
+			print("5. Save")
+			print("6. Exit")
 			choice = input("Enter Your Choice: ")
+			if choice=='1':
+				pass
+			elif choice=='2':
+				pass
+			elif choice=='3':
+				print("Your Portfolio:")
+				print(f"Name: {name}")
+				print(f"Current Balance: ${balance:.2f}")
+				print("Current Stocks:", end="")
+				for stock in stocks:
+					print("Stock: {stock[0]}:{stock[1]}", end="")
+				print(f"\nDate Of Birth: {dob}")
+				print(f"Email Address: {email}")
+				print(f"Phone Number: {phoneno}")
+				input("Press Enter to continue...")
+			elif choice=='4':
+				print("Enter Your New Personal Information:")
+				name=input("Name Of Your Portfolio: ")
+				dob=input("Date Of Birth(dd/mm/yyyy): ")
+				email=input("Email Address: ")
+				phoneno=input("Phone Number: ")
+				save(savename, name, balance, stocks, dob, email)
+				print("Personal Information Updated")
+				input("Press Enter to continue...")
+			elif choice=='5':
+				save(savename, name, balance, stocks, dob, email)
+				print("Changes Saved")
+				input("Press Enter to continue...")
+			elif choice=='6':
+				confirm = input("Are You Sure You Want To Exit [y/n]: ").lower()
+				if confirm=='y':save(savename, name, balance, stocks, dob, email)
+				print("Changes Saved, Thank For Playing")
+				time.sleep(1)
+				exit(0)
+			else:
+				print("Invalid Choice")
+				time.sleep(1)
